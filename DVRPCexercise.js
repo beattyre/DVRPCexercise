1d2021b15fa105197b24307b72377dfb7f0ab761
beattyre <beattyre@miamioh.edu>
2017-03-12 14:47:25 -0400

    Update DVRPCexercise.js

--- DVRPCexercise.js
+++ DVRPCexercise.js
@@ -1,2 +1,15 @@
-var map= L.map('map').setView([X, Y], View) //Find X,Y for Philadelphia, set default view
-
+var map;
+function initmap() {
+  map = new L.map('map');
+  // Add basemap from OSM
+  var osmHot = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+  var osmAttributes = 'Tile layers provided by <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>, data supplied by the<a href="www.opendataphilly.org">City of Philadelphia.</a>'
+    minZoom: 15,
+    maxZoom: 19,
+    attribution: osmAttributes,
+    zoomControl: true
+  });
+  map.setView(new L.LatLng(39.9532, -75.1520), 13);
+  map.addLayer(osm);
+};
+initmap();
